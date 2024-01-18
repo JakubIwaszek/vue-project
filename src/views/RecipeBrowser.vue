d64449edf8f2e3706853a297396f949b18767feb
Jakub Iwaszek <s223387@student.uek.krakow.pl>
2024-01-18 13:43:59 +0100

    fixed indents issue

--- src/views/RecipeBrowser.vue
+++ src/views/RecipeBrowser.vue
@@ -13,23 +13,23 @@ var recipes = <Recipe>{};
 var selectedRecipe = useLocalStorage<Recipe>('recipe', {});
 
 const fetchRecipes = async () => {
-  const { data: dataFirst } = await axios<RecipeResults>('https://themealdb.com/api/json/v1/1/search.php?f=a', {});
-  const { data: dataSecond } = await axios<RecipeResults>('https://themealdb.com/api/json/v1/1/search.php?f=b', {});
+	const { data: dataFirst } = await axios<RecipeResults>('https://themealdb.com/api/json/v1/1/search.php?f=a', {});
+	const { data: dataSecond } = await axios<RecipeResults>('https://themealdb.com/api/json/v1/1/search.php?f=b', {});
 	const allMeals = [...dataFirst.meals, ...dataSecond.meals];
 
-  recipesDisplay.value = allMeals;
+	recipesDisplay.value = allMeals;
 	recipes = allMeals;
 };
 
 const setRecipe = (recipe: Recipe) => {
 	selectedRecipe.value = recipe;
 	router.push({
- 		name: 'Recipe details'
+		name: 'Recipe details'
 	});
 };
 
 onMounted(() => {
-  document.title = 'Browse recipes';
+	document.title = 'Browse recipes';
 });
 
 fetchRecipes();
@@ -37,14 +37,15 @@ fetchRecipes();
 </script>
 
 <template>
-    <main class="bg-rose-200">
-        <div class="grid grid-cols-3 gap-4 p-4">
-					<div v-for="singleRecipe in recipesDisplay" :key="recipesDisplay.strMeal" class="relative cursor-pointer hover:scale-105" @click="setRecipe(singleRecipe)">
-							<img :src="singleRecipe.strMealThumb" alt="Image" class="w-full h-48 object-cover mb-2 rounded" />
-							<div class="absolute bottom-2 left-0 right-0 bg-gray-800 bg-opacity-50 text-white p-2">
-								{{ singleRecipe.strMeal }}
-							</div>
-					</div>
-        </div>
-		</main>
+	<main class="bg-rose-200">
+		<div class="grid grid-cols-3 gap-4 p-4">
+			<div v-for="singleRecipe in recipesDisplay" :key="recipesDisplay.strMeal"
+				class="relative cursor-pointer hover:scale-105" @click="setRecipe(singleRecipe)">
+				<img :src="singleRecipe.strMealThumb" alt="Image" class="w-full h-48 object-cover mb-2 rounded" />
+				<div class="absolute bottom-2 left-0 right-0 bg-gray-800 bg-opacity-50 text-white p-2">
+					{{ singleRecipe.strMeal }}
+				</div>
+			</div>
+		</div>
+	</main>
 </template>
